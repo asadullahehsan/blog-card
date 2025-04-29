552388bdef92be7d0a3f27a9e90ff8fccb445a11
Asadullah Ehsan <asadullahehsan10@gmail.com>
2025-04-29 19:22:58 +0500

    add noto sans font

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
